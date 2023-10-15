925308cf782ec119f92ebc97e0f9594ecb2f540f
Vladlen Galetsky <83239306+terrordayvg@users.noreply.github.com>
2023-10-15 19:17:02 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Hello world")
+# Create test_process_tomography_code for certain base functions
+import process_tomography_code as po
+  
+def test_dec_to_bin(): #From decimal to binary N-bits string.
+  # test 5 into 101
+  # x=5, N=3
+  x=5
+  N=3
+  res=po.dec_to_bin(x,N)
+  assertEqual(res, "101")
+  
+if __name__ == '__main__':
+   test_dec_to_bin()
